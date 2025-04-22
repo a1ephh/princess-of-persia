5f4a1de98275dbc4fe4163a398cf340927fc1120
Jana <jana.mosalama@aucegypt.edu>
2025-04-22 16:03:43 +0200

    Add sprites images and update resources.qrc file

--- main.cpp
+++ main.cpp
@@ -1,53 +1,28 @@
 #include "mainwindow.h"
-
 #include <QApplication>
-#ifndef OBSTACLE_H
-#define OBSTACLE_H
-
-#include <QString>
-
-class Obstacle {
-private:
-    QString name;
-    bool lethal;
-    bool solid;
-
-public:
-    // Constructor
-    Obstacle(QString name, bool lethal, bool solid)
-        : name(name), lethal(lethal), solid(solid) {}
-
-    // Getters
-    QString getName() const { return name; }
-    bool isLethal() const { return lethal; }
-    bool isSolid() const { return solid; }
-
-    // Setters (if needed)
-    void setName(QString n) { name = n; }
-    void setLethal(bool l) { lethal = l; }
-    void setSolid(bool s) { solid = s; }
-
-    // Example behavior method
-    QString interact() const {
-        if (lethal)
-            return "You died!";
-        else if (solid)
-            return "You can't pass!";
-        else
-            return "You pass safely.";
-    }
-};
-
-#endif // OBSTACLE_H
+#include "obst.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
-    // hello this is a comment
-    // hellooooo
-    //janna was here
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    // Set up scene and view
+    QGraphicsScene* scene = new QGraphicsScene();
+    QGraphicsView* view = new QGraphicsView(scene);
+    view->setFixedSize(800, 600);
+    scene->setSceneRect(0, 0, 800, 600);
+
+    // Example use: create obstacle
+    Obst* spike = new Obst(":/sprites/spike.png");
+    spike->setType(Obst::Spike);
+    spike->setPos(200, 300);
+    scene->addItem(spike);
+
+    view->show();
+
     return a.exec();
-    //jana
 }
+
+
