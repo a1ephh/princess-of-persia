e4b8c765eec102d10acea7241c4b7190ab6861e0
asmaa641 <asmaaelshabshiri@aucegypt.edu>
2025-04-25 21:05:11 +0300

    Added code for game_status class

---
+++ game_status.cpp
@@ -0,0 +1,26 @@
+#include "game_status.h"
+#include <QDebug>
+
+game_status::game_status(QObject *parent)
+    : QObject(parent), currentLevel(1) {}
+
+void game_status::startGame() {
+    currentLevel = 1;
+    emit levelChanged(currentLevel);
+    qDebug() << "Game started. Current level:" << currentLevel;
+}
+
+void game_status::restartLevel() {
+    emit levelChanged(currentLevel);
+    qDebug() << "Restarting Level" << currentLevel;
+}
+
+int game_status::getCurrentLevel() const {
+    return currentLevel;
+}
+
+void game_status::nextLevel() {
+    currentLevel++;
+    emit levelChanged(currentLevel);
+    qDebug() << "Proceeding to Level" << currentLevel;
+}
--- game_status.h
+++ game_status.h
@@ -1,4 +1,24 @@
 #ifndef GAME_STATUS_H
 #define GAME_STATUS_H
 
+#include <QObject>
+
+class game_status : public QObject {
+    Q_OBJECT
+
+private:
+    int currentLevel;
+
+public:
+    explicit game_status(QObject *parent = nullptr);
+
+    void startGame();
+    void restartLevel();
+    int getCurrentLevel() const;
+    void nextLevel();
+
+signals:
+    void levelChanged(int newLevel);
+};
+
 #endif // GAME_STATUS_H
