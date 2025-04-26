78da9721de1c8f1a30dffc17adc3f19ee2899679
Jana <jana.mosalama@aucegypt.edu>
2025-04-26 22:48:57 +0300

    edited obstacle class

--- main.cpp
+++ main.cpp
@@ -22,6 +22,28 @@ int main(int argc, char *argv[])
 
     view->show();
 
+    // Create an obstacle
+    Obst* bladeObstacle = new Obst(":/sprites/blade1.png");
+
+    // Set its type
+    bladeObstacle->setType(Obst::Blade);
+
+    // Enable animation (will cycle through frames)
+    bladeObstacle->enableAnimation(true);
+
+    // Set position in the scene
+    bladeObstacle->setPos(300, 150); // x=300, y=150
+
+    // Add to the scene
+    scene->addItem(bladeObstacle);
+
+    Obst* spikeObstacle = new Obst(":/sprites/spike.png");
+    spikeObstacle->setType(Obst::Spike);
+    spikeObstacle->setPos(500, 400);
+    scene->addItem(spikeObstacle);
+
+
+
     return a.exec();
 }
 
